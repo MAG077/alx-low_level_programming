d4e3bd1143efcddfdcfc8d06eb038d6d4ec12503
mag <mag@example.com>
2023-08-11 15:01:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - 4-puts.c
+ * description - write a C program that prins exactly
+ *
+ * Return: always (0) (success);
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return;
+}
